79ef41afc592550fadff89f766b008bbadff4b3e
Ubuntu <ubuntu@ip-172-31-77-23.ec2.internal>
2018-05-29 03:15:05 +0000

    added logic to autopopulate environment variables in run.py

--- project/server/models.py
+++ project/server/models.py
@@ -6,7 +6,6 @@ import datetime
 
 from project.server import app, db, bcrypt
 
-
 class User(db.Model):
     """ User Model for storing user related details """
     __tablename__ = "users"
--- run.py
+++ run.py
@@ -1,9 +1,18 @@
 #!venv/bin/python
 from logging import FileHandler, WARNING
+
+# Adding 2 environment variables before importing app config.
+import os
+os.environ["SECRET_KEY"] = "curatus_temp_pw_key"
+os.environ["APP_SETTINGS"] = "project.server.config.ProductionConfig"
+
+
 from manage import app
 
 file_handler = FileHandler('./project/logs/error_log.txt')
 file_handler.setLevel(WARNING)
+
+print(app.config.get('SECRET_KEY'))
 
 app.logger.addHandler(file_handler)
 app.run(debug=False,host='0.0.0.0',port=5000)
